bdf8229aa6e898556cf307f329a979f554a65703
Redeemm <71359434+Redeemm@users.noreply.github.com>
2022-08-17 02:11:00 +0000

    .

--- src/Stack_Queue_Project/queue.java
+++ src/Stack_Queue_Project/queue.java
@@ -4,69 +4,68 @@ import java.util.Arrays;
 
 public class queue {
 
-	private int front;
-	private int rear;
-	private final char[] queueArray;
-	private int currentQueueSize;
+    private final char[] queueArray;
+    private int front;
+    private int rear;
+    private int currentQueueSize;
 
-	// Constructor
-	queue(int maxSize) {
-		queueArray = new char[maxSize];
-		front = -1; // (maxSize + 1) - 1
-		rear = -1;  // (maxSize + 1) - 1
-		currentQueueSize = 0;
-	}
+    // Constructor
+    queue(int maxSize) {
+        queueArray = new char[maxSize];
+        front = -1; // (maxSize + 1) - 1
+        rear = -1;  // (maxSize + 1) - 1
+        currentQueueSize = 0;
+    }
 
-	// Pushes a character to the 'rear' of the queue array
-	public void enqueue(char character){
-		if(isFull()) {
-			System.out.println("Queue is full. No elements can be added.");
-		}
-		else {
-			rear = (rear + 1) % queueArray.length;
-			queueArray[rear] = character;
-			currentQueueSize++;
-		}
-		if(front == -1)
-			front++;
-	}
+    // Pushes a character to the 'rear' of the queue array
+    public void enqueue(char character) {
+        if (isFull()) {
+            System.out.println("Queue is full. No elements can be added.");
+        } else {
+            rear = (rear + 1) % queueArray.length;
+            queueArray[rear] = character;
+            currentQueueSize++;
+        }
+        if (front == -1)
+            front++;
+    }
 
-	// Moves the character to 'front' position, assigns to variable and
-	// decrements the available size of the array;
-	public char dequeue() {
-		char dequeuedCharacter = '\0';
+    // Moves the character to 'front' position, assigns to variable and
+    // decrements the available size of the array;
+    public char dequeue() {
+        char dequeuedCharacter = '\0';
 
-		if(isEmpty())
-			System.out.println("The queue is empty. Cannot remove anymore elements.");
+        if (isEmpty())
+            System.out.println("The queue is empty. Cannot remove anymore elements.");
 
-		else {
-			dequeuedCharacter = queueArray[front];
-			queueArray[front] = '\0';
-			front = (front + 1) % queueArray.length;
-			--currentQueueSize;
-		}
-		return dequeuedCharacter;
-	}
+        else {
+            dequeuedCharacter = queueArray[front];
+            queueArray[front] = '\0';
+            front = (front + 1) % queueArray.length;
+            --currentQueueSize;
+        }
+        return dequeuedCharacter;
+    }
 
-	// Checks to see if the array is empty
-	public boolean isEmpty() {
-		return currentQueueSize == 0;
-	}
+    // Checks to see if the array is empty
+    public boolean isEmpty() {
+        return currentQueueSize == 0;
+    }
 
-	// Checks to see if the array is full
-	public boolean isFull() {
-		return currentQueueSize == queueArray.length;
-	}
+    // Checks to see if the array is full
+    public boolean isFull() {
+        return currentQueueSize == queueArray.length;
+    }
 
-	// Clear the 'queue' of all array contents
-	public void clearArray() {
-		//queueArray = null;
-		Arrays.fill(queueArray, '\0');
-	}
+    // Clear the 'queue' of all array contents
+    public void clearArray() {
+        //queueArray = null;
+        Arrays.fill(queueArray, '\0');
+    }
 
-	@Override
-	public String toString() {
-		return "queue [front=" + front + ", rear=" + rear + ", queueArray=" + Arrays.toString(queueArray)
-				+ ", currentQueueSize=" + currentQueueSize + "]";
-	}
+    @Override
+    public String toString() {
+        return "queue [front=" + front + ", rear=" + rear + ", queueArray=" + Arrays.toString(queueArray)
+                + ", currentQueueSize=" + currentQueueSize + "]";
+    }
 } // end class
--- src/Stack_Queue_Project/stack.java
+++ src/Stack_Queue_Project/stack.java
@@ -4,72 +4,72 @@ import java.util.Arrays;
 
 public class stack {
 
-	private final int maxSize;
-	private int ptrNum;	// the location of the pointer; top of the stack
-	private final char[] stackArray;
+    private final int maxSize;
+    private final char[] stackArray;
+    private int ptrNum;    // the location of the pointer; top of the stack
 
-	// Constructor
-	public stack(int capacity) { // Use 'capacity' for number of elements and remove 'maxSize' as a data member
-		maxSize = capacity;
-		stackArray = new char[maxSize];
-		ptrNum = -1;
-	}
+    // Constructor
+    public stack(int capacity) { // Use 'capacity' for number of elements and remove 'maxSize' as a data member
+        maxSize = capacity;
+        stackArray = new char[maxSize];
+        ptrNum = -1;
+    }
 
-	// Checks to see if the array is empty
-	public boolean isEmpty() {
-		return ptrNum < 0;
-	}
+    // Checks to see if the array is empty
+    public boolean isEmpty() {
+        return ptrNum < 0;
+    }
 
-	// Checks to see if the array is full
-	public boolean isFull() {
-		return ptrNum == (maxSize - 1);
-	}
+    // Checks to see if the array is full
+    public boolean isFull() {
+        return ptrNum == (maxSize - 1);
+    }
 
-	// Reset the stack ptrNum to top of stack
-	  public void resetPointer() {
-		  ptrNum = maxSize - 1;
-	 }
+    // Reset the stack ptrNum to top of stack
+    public void resetPointer() {
+        ptrNum = maxSize - 1;
+    }
 
-	// Push method pushes a value onto the stack
-	public void push(char ch) {
-		if(!this.isFull()) {
-			ptrNum++;
-			stackArray[ptrNum] = ch;
-		}
-	}
+    // Push method pushes a value onto the stack
+    public void push(char ch) {
+        if (!this.isFull()) {
+            ptrNum++;
+            stackArray[ptrNum] = ch;
+        }
+    }
 
-	// Pop method 'pops' a value off the stack and decrements the value of ptrNum
-	public char pop() {
+    // Pop method 'pops' a value off the stack and decrements the value of ptrNum
+    public char pop() {
 
-		char character;
+        char character;
 
-		if(this.isEmpty()) {
-			System.out.println("The stack is empty.");
-			return '\0';
+        if (this.isEmpty()) {
+            System.out.println("The stack is empty.");
+            return '\0';
 
-		} else {
-			character = stackArray[ptrNum];
-			ptrNum--;
-		}
-		return character;
-	}
+        } else {
+            character = stackArray[ptrNum];
+            ptrNum--;
+        }
+        return character;
+    }
 
-	// Top method returns the value of the 'top' of the stack but does not decrement
-	public char top() {
-		char temp = '0';
-		if(ptrNum > -1)
-			temp = stackArray[ptrNum];
-		return temp;
-	}
+    // Top method returns the value of the 'top' of the stack but does not decrement
+    public char top() {
+        char temp = '0';
+        if (ptrNum > -1)
+            temp = stackArray[ptrNum];
+        return temp;
+    }
 
-	// Clear the 'stack' of all array contents
-	public void clearArray() {
-		//stackArray = null;
-		Arrays.fill(stackArray, '\0');
-	}
+    // Clear the 'stack' of all array contents
+    public void clearArray() {
+        //stackArray = null;
+        Arrays.fill(stackArray, '\0');
+    }
 
-	@Override
-	public String toString() {
-		return "stack [maxSize=" + maxSize + ", ptrNum=" + ptrNum + ", stackArray=" + Arrays.toString(stackArray) + "]";
-	}
+    @Override
+    public String toString() {
+        return "stack [maxSize=" + maxSize + ", ptrNum=" + ptrNum + ", stackArray=" + Arrays.toString(stackArray) + "]";
+    }
 }
